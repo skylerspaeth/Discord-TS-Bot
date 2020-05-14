b37bcce97417855579ce133ac6b6e01a8531f806
Jon K <helix@vega>
2020-05-13 19:43:17 -0500

    Added customizable prefixes, added ability to display user's Avatar URL, added to-do

--- main.js
+++ main.js
@@ -3,16 +3,15 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const chalk = require('chalk');
 const {token} = require('./config.json');
-console.log(token);
 
 const soundbytes = ["boi", "cringingme", "earrape", "evan", "everyword", "fam", "getout", "hacking", "idk", "instruments", "kys", "mom", "nsfw", "portal2", "smiling", "spam", "sparticus", "trigger", "water", "wetalked"];
 
 
 try {
-        client.login(token);
-        console.log('Ready');
+  client.login(token);
+  console.log('Ready');
 } catch {
-        console.log('Failed');
+  console.log('Failed');
 }
 
 function generateUrl(filename) {
@@ -25,31 +24,47 @@ client.on('message', async message => {
   
   if (!message.guild) return;
 
-  let command = message.content.split(' ')[0];
+  let prefix = "???";
+  let prefixLen = prefix.length;
+
+  if (message.content.substring(0,prefixLen) === prefix) {
+    console.log("YEET");
+    console.log(message.content.substring(0,prefixLen));
+  }
+  let command = message.content.split(prefix)[1];
   let param = message.content.split(' ')[1];
 
-  if (command === '//play') {
-    console.log(`${chalk.blue(message.content)} command issued by ${chalk.yellow(message.author.username)}`);
+  if (command === 'display' && param === "avatar") {
+    // Send the user's avatar URL
+    message.reply(message.author.displayAvatarURL());
+  }
 
+  if (command === 'play') {
+    console.log(`${chalk.blue(message.content)} command issued by ${chalk.yellow(message.author.username)}#${chalk.yellow(message.author.discriminator)}`);
+  
     // Only try to join the sender's voice channel if they are in one themselves
     if (message.member.voice.channel) {
-                var currentChannel = message.member.voice.channel;
-                const connection = await currentChannel.join();
-                connection.play(generateUrl(param));
-                //var connection = await message.member.voice.channel.leave();
-
-    } else {
-      message.reply('You need to join a voice channel first!');
-    }
-  } else if (command === '//sounds') {
-    console.log(`${chalk.green(message.content)} command issued by ${chalk.yellow(message.author.username)}`);
+      var currentChannel = message.member.voice.channel;
+      const connection = await currentChannel.join();
+      connection.play(generateUrl(param));
+      // var connection = await message.member.voice.channel.leave();
+  
+    } else { message.reply('You need to join a voice channel first!'); }
+    
+  } else if (command === 'sounds') {
+    console.log(`${chalk.green(message.content)} command issued by ${chalk.yellow(message.author.username)}#${chalk.yellow(message.author.discriminator)}`);
     message.reply(`Available soundbytes are: ${soundbytes.join(', ')}`);
-  } else if (command === '//addrole') {
+  } else if (command === 'addrole') {
     message.guild.roles.create({
-      data: { name: param, color: 'purple' }, reason: 'Command addrole executed'
+      data: {
+         name: param,
+         color: 'purple' 
+      }, reason: 'Command addrole executed'
     })
       .then(console.log).catch(console.error); 
-  } 
+  } else if (command === 'ts') {
+    
+  }
 });
 
 
@@ -61,4 +76,19 @@ client.on('message', async message => {
              Guild.createRole("TS Holder");
              Guild.createRole("TS Muted");
         }
+
+        let prefix = "//";
+        let command = message.content.split(prefix)[1]
+
 */ 
+
+/*To Do
+        Incorporate permissions in roles
+        Set master role for moderators to override the bot's muting, as well as who has the talking stick
+        For each iterate mute active voice channel except for message.author
+        Allow the original message.author pass the Talking role to another member of the current voice channel
+        Auto add and remove roles as users join and leave the applied voice channel
+        Record Audio
+        Allow users to set marks in the audio with a command, and allow custom names on these markings
+        Play YouTube URLs
+*/
